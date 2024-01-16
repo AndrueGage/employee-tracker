b35f59b14b8f584c7c9293a748f1589a0d87998a
AndrueGage <trainer.andrue@gmail.com>
2024-01-16 14:47:34 -0600

    start function

--- server.js
+++ server.js
@@ -11,3 +11,23 @@ const db = mysql.createConnection(
     console.log(`Connected to the employees_db database.`)
 );
 
+function start {
+    inquirer
+        .prompt({
+            type: 'list',
+            name: 'task',
+            message: 'What would you like to do?',
+            choices: [
+                'View All Employees',
+                'Update Employee Role',
+                'View All Roles',
+                'Add Roles',
+                'View All Departments',
+                'Add Deparment',
+                'Add An Employee',
+                'Update An Employee Role',
+                'Quit'
+            ]
+        })
+};
+
